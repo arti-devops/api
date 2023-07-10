702a95b69e2b841ea15e5c4ba3c1ba4182aad426
Alassane Cissé <grenciss@192.168.1.4>
2023-07-10 22:28:41 +0000

    Normalize database name, log to logs

--- config/db.py
+++ config/db.py
@@ -1,7 +1,8 @@
 import pymongo
 #TODO in dev, adapt this link
-conn = pymongo.MongoClient("mongodb://192.168.0.9",port=27017)
+#conn = pymongo.MongoClient("mongodb://192.168.0.9",port=27017)
+conn = pymongo.MongoClient("mongodb://localhost",port=27017)
 
 # Documents
 devicesdb = conn.dsi.devices
-logsdb = conn.dsi.log
+logsdb = conn.dsi.logs
