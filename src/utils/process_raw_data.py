6102c9526381cd9491599398641e650b60e9a3ed
ARTI DevOps <grencisse@hotmail.com>
2023-07-12 15:05:05 +0200

    Add raw data processing pipe

---
+++ src/utils/process_raw_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def filter_raw_log_data(rw, filter) -> pd.DataFrame: 
+    """Reads csv file"""
+
+    # Read Data Sources
+    df_rw = pd.read_csv(rw, encoding="latin-1")  
+    df_filter = pd.read_csv(filter, encoding="latin-1") 
+
+    noms_a = df_filter["noms"].tolist()  
+    matricules_a = df_filter["MATRICULE"].tolist()  
+
+    df_rw = df_rw[df_rw["Name"].isin(noms_a)]  
+
+    df_rw["Matricule"] = df_rw["Name"].map(dict(zip(noms_a, matricules_a)))
+
+    # Enregistrer le résultat dans un nouveau fichier Excel
+    #df_rw.to_csv("fichier_b_filtre.csv", index=False) 
+    return df_rw
