95a4962c8d9e8b2a5751ebcbbf691d7fa0dc08f8
Alassane Cissé <grenciss@192.168.1.3>
2023-07-12 07:15:34 +0000

    Update device schema, add attribute validator

--- src/modules/dsi/schemas/device.py
+++ src/modules/dsi/schemas/device.py
@@ -1,14 +1,16 @@
+from src.utils.validate_schema import *
+
 def deviceEntity(item) -> dict:
     return{
         "device_id":str(item["_id"]),
-        "device_user":str(item["device_user"]),
-        "device_type":str(item["device_type"]),
-        "device_post_number":str(item["device_post_number"]),
-        "device_brand_name":str(item["device_brand_name"]),
-        "device_brand_model":str(item["device_brand_model"]),
-        "device_ip_address":str(item["device_ip_address"]),
-        "device_status":str(item["device_status"]),
-        "device_serial_number":str(item["device_serial_number"]),
+        "device_user":validate_schema_str_attr(item,"device_user"),
+        "device_type":validate_schema_str_attr(item,"device_type"),
+        "device_post_number":validate_schema_int_attr(item,"device_post_number"),
+        "device_brand_name":validate_schema_str_attr(item,"device_brand_name"),
+        "device_brand_model":validate_schema_str_attr(item,"device_model_name"),
+        "device_ip_address":validate_schema_str_attr(item,"device_ip_address"),
+        "device_status":validate_schema_str_attr(item,"device_status"),
+        "device_serial_number":validate_schema_str_attr(item,"device_serial_number")
     }
 
 def devicesEntity(entity) -> list:
